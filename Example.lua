1d9a430790758e2bd6c46ba34236f34c86d1519c
DataStoreGen <lk6394785@gmail.com>
2024-09-16 21:41:02 -0800

    Update Example.lua
    an small example of how to use it

--- Example.lua
+++ Example.lua
@@ -1,1 +1,19 @@
+local GetData = require(script.Parent.GetData)
+local Replicated = game:GetService('ReplicatedStorage')
+local StringLibrary = require(Replicated:FindFirstChild('StLibrary'))
+local ClickSend = Replicated:FindFirstChild('ClickSend')
+print(StringLibrary.Percent(1, 100))
 
+ClickSend.OnServerInvoke = function(player, message)
+	local data = GetData.GetData(player)
+	if message == 'Click Add' then
+		local Stat_s = player:FindFirstChild('Stats')
+		local Clicks = Stat_s:FindFirstChild('Clicks')
+		local add = StringLibrary.add(data['Clicks'], data['ClickPlus'])
+		data['Clicks'] = add
+		Clicks.Value = data['Clicks']
+	else
+		data['Ban'] = player.Name
+	end
+end
+-- to keep it secure well not the Event the Value Changing part
